5020290c0586f63b55cacd71e672afa4b35d3c44
Richelmy <richelmy.monteiro@gmail.com>
2020-02-11 19:36:15 -0300

    Isolado o OK da bandeja 1

--- scraper.py
+++ scraper.py
@@ -1,19 +1,22 @@
 import requests
 from bs4 import BeautifulSoup
 
+endereco_impressora = '10.127.244.21'
+url_printer_status = ''
 
 # Coletar a primeira página da lista de artistas
 page = requests.get('http://10.127.244.21/cgi-bin/dynamic/printer/PrinterStatus.html')
 soup = BeautifulSoup(page.text, 'html.parser')
-# print(soup)
-tabela = soup.findAll("table", {"class": "status_table"})
-b_tag = tabela[2].find_all("b")
-print(b_tag)
+"""
+Status de suprimento de papel fica específicamente sob o estilo
+padding: .75pt
+0 = Bandeja 1
+1 = Bandeja 2
+2 = MF Alimentador
+3 = Bandeja Padrão
 
 """
-contador = 0
-for b in b_tag:
-	print(b)
-	contador += 1
-	print("contador: ",contador)
-"""
+resultado = soup.findAll("table", {"style": "padding: .75pt"})
+bandeja_1 = resultado[0].find_all("b")
+print(bandeja_1[0].contents[0])
+# finalmente consegui isolar o "ok"
