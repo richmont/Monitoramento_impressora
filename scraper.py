64be893b4f4dca5f1334c28719e77c1b107162f5
Richelmy <richelmy.monteiro@gmail.com>
2020-02-13 20:39:02 -0300

    Scraper separado em funções e retornando texto puro

--- scraper.py
+++ scraper.py
@@ -1,13 +1,12 @@
 import requests
 from bs4 import BeautifulSoup
 
-endereco_impressora = '10.127.244.21'
-url_printer_status = ''
-
+ip = '10.127.244.21'
+"""
 # Coletar a primeira página da lista de artistas
 page = requests.get('http://10.127.244.21/cgi-bin/dynamic/printer/PrinterStatus.html')
 soup = BeautifulSoup(page.text, 'html.parser')
-"""
+
 Status de suprimento de papel fica específicamente sob o estilo
 padding: .75pt
 0 = Bandeja 1
@@ -16,7 +15,38 @@ padding: .75pt
 3 = Bandeja Padrão
 
 """
-resultado = soup.findAll("table", {"style": "padding: .75pt"})
-bandeja_1 = resultado[0].find_all("b")
-print(bandeja_1[0].contents[0])
 # finalmente consegui isolar o "ok"
+
+
+def scraper_pagina(ip):
+    """
+    Recebe um endereço e retorna um objeto beautifulsoup
+    """
+    endereco = str('http://' + ip + '/cgi-bin/dynamic/printer/PrinterStatus.html')
+    pagina = requests.get(endereco)
+    soup = BeautifulSoup(pagina.text, 'html.parser')
+    return soup
+
+
+def filtragem(soup):
+    """
+    recebe um objeto beautifulsoup
+    retorna 4 variáveis com o status das bandejas em forma de texto
+    """
+
+    resultado = soup.findAll("table", {"style": "padding: .75pt"})
+    bandeja_1 = resultado[0].find_all("b", text=True)[0].text
+    bandeja_2 = resultado[1].find_all("b", text=True)[0].text
+    mf_alimentador = resultado[2].find_all("b", text=True)[0].text
+    bandeja_padrao = resultado[3].find_all("b", text=True)[0].text
+    return bandeja_1, bandeja_2, mf_alimentador, bandeja_padrao
+
+
+soup = scraper_pagina(ip)
+bandeja_1, bandeja_2, mf_alimentador, bandeja_padrao = filtragem(soup)
+"""
+print("Bandeja 1: ", bandeja_1)
+print("Bandeja 2: ", bandeja_2)
+print("MF Alimentador: ", mf_alimentador)
+print("Bandeja Padrão: ", bandeja_padrao)
+"""
