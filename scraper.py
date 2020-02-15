a79adef2fd175594769c255e81fdf9417e868fd4
Richelmy <richelmy.monteiro@gmail.com>
2020-02-14 22:17:24 -0300

    Scraper limpo para receber IP diretamente e retornar apenas o essencial

--- scraper.py
+++ scraper.py
@@ -1,9 +1,8 @@
 import requests
 from bs4 import BeautifulSoup
 
-ip = '10.127.244.21'
+
 """
-# Coletar a primeira página da lista de artistas
 page = requests.get('http://10.127.244.21/cgi-bin/dynamic/printer/PrinterStatus.html')
 soup = BeautifulSoup(page.text, 'html.parser')
 
@@ -15,17 +14,19 @@ padding: .75pt
 3 = Bandeja Padrão
 
 """
-# finalmente consegui isolar o "ok"
 
 
 def scraper_pagina(ip):
     """
     Recebe um endereço e retorna um objeto beautifulsoup
     """
-    endereco = str('http://' + ip + '/cgi-bin/dynamic/printer/PrinterStatus.html')
-    pagina = requests.get(endereco)
-    soup = BeautifulSoup(pagina.text, 'html.parser')
-    return soup
+    try:
+        endereco = str('http://' + ip + '/cgi-bin/dynamic/printer/PrinterStatus.html')
+        pagina = requests.get(endereco)
+        soup = BeautifulSoup(pagina.text, 'html.parser')
+        return soup
+    except requests.exceptions.RequestException:
+        print("Verifique sua conexão ou o endereço IP da impressora")     
 
 
 def filtragem(soup):
@@ -33,17 +34,24 @@ def filtragem(soup):
     recebe um objeto beautifulsoup
     retorna 4 variáveis com o status das bandejas em forma de texto
     """
+    try:
+        resultado = soup.findAll("table", {"style": "padding: .75pt"})
+        bandeja_1 = resultado[0].find_all("b", text=True)[0].text
+        bandeja_2 = resultado[1].find_all("b", text=True)[0].text
+        # mf_alimentador = resultado[2].find_all("b", text=True)[0].text
+        # bandeja_padrao = resultado[3].find_all("b", text=True)[0].text
+        return bandeja_1, bandeja_2  # , mf_alimentador, bandeja_padrao
+    except AttributeError:
+        raise AttributeError("Verifique o objeto beautifulsoup recebido pela função")
 
-    resultado = soup.findAll("table", {"style": "padding: .75pt"})
-    bandeja_1 = resultado[0].find_all("b", text=True)[0].text
-    bandeja_2 = resultado[1].find_all("b", text=True)[0].text
-    mf_alimentador = resultado[2].find_all("b", text=True)[0].text
-    bandeja_padrao = resultado[3].find_all("b", text=True)[0].text
-    return bandeja_1, bandeja_2, mf_alimentador, bandeja_padrao
+def executar_verificacao(ip):
+    soup = scraper_pagina(ip)
+    bandeja_1, bandeja_2 = filtragem(soup)
+    
 
 
-soup = scraper_pagina(ip)
-bandeja_1, bandeja_2, mf_alimentador, bandeja_padrao = filtragem(soup)
+executar_verificacao('10.127.244.22')
+
 """
 print("Bandeja 1: ", bandeja_1)
 print("Bandeja 2: ", bandeja_2)
